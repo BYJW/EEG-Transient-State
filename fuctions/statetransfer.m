58ad8b5bbf5881ec94540df011432b0b2a0e8024
Yang Bai <30861587+BYJW@users.noreply.github.com>
2021-06-28 15:40:07 +0200

    Update statetransfer.m

--- fuctions/statetransfer.m
+++ fuctions/statetransfer.m
@@ -1,6 +1,25 @@
 function [Gamma, vpath, hmm]=statetransfer(data,T,options,h)
 
+%%
 
+% The script corresponding to the transfer analysis used in manuscript 'Spontaneous transient brain states in EEG source space of disorders of consciousness'
+% The underlying details could be found in the supplementary information
+% an OSL and HMM toolbox are needed, which can be accessed from https://ohba-analysis.github.io/osl-docs/ and https://github.com/OHBA-analysis/HMM-MAR/wiki
+% This script was modified from a pipeline: Vidaurre, D., Hunt, L. T., Quinn, A. J., Hunt, B. a. E., Brookes, M. J., Nobre, A. C. & Woolrich, M. W. 2018. Spontaneous cortical activity transiently organises into frequency specific phase-coupling networks. Nat Commun, 9, 2987.
+% The original pipeline on MEG analysis could be found from https://github.com/OHBA-analysis/HMM-MAR/tree/master/examples
+
+% Input: data (ROIs * time points), T (length of each session/epoch of single subject)
+% options       structure with the training options - see documentation in 
+%                       https://github.com/OHBA-analysis/HMM-MAR/wiki
+% OUTPUT:
+% hmm           estimated HMMMAR model
+% Gamma         Time courses of the states probabilities given data
+% vpath         most likely state path of hard assignments
+
+
+%  edit by Yang Bai 2021-06-22
+
+%%
 %%%%%to do the preprocessing includes the PCA of the data
 N = length(T);
 dat = cell(N,1); TT = cell(N,1);
